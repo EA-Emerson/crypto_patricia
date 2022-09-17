11a654a0af805feada56188900db1b6279bf6399
Aghogho Emokiniovo <emokiniovoaghogho@gmail.com>
2022-09-17 10:38:27 +0100

    second commit

--- script.js
+++ script.js
@@ -15,42 +15,4 @@ menu.addEventListener('click', function(e){
         mini_nav.style.display = 'none';
         menu.src = ('img/menu-button-of-three-horizontal-lines.png');
     }
-    // mini_nav.style.display = 'block';
-    // menu.src = ('img/menu close.png');
-    // menu.addEventListener('click', function(e){
-    //     mini_nav.style.display = 'none';
-    //     menu.src = "img/menu-button-of-three-horizontal-lines.png";
-
-    //     return
-    // })
 })
-
-// var menu=document.querySelector('.menulogoimg')
-// menu.addEventListener('click', function(e){
-//     e.preventDefault()
-    
-//     var ul=document.querySelector('.hidden_ul')
-//     if (ul.style.display=='none') {
-//         ul.style.display='block';
-//         menu.style.display='none'
-
-//     }
-//     else{
-//         ul.style.display='none';
-//     }
-//     ul.addEventListener('click', function(a){
-//         // a.preventDefault()
-//         var close=document.querySelector('#nav_close')
-//         close.addEventListener('click', function(c){
-//         if(ul.style.display=='block'){
-//             menu.style.display='block';
-//             ul.style.display='none'
-//         }
-//         else{
-//             ul.style.display='none'
-//         }
-//         })
-
-//     })
-
-// })
